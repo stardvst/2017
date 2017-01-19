e305e85406998a3939c0197956ee5dce2d63e5fd
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-01-19 17:43:21 +0400

    function object - 2 (std::less)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <functional>
 
-class case_insensitive_cmp {
-public:
-  bool is_less_than(const std::string& lhs, const std::string& rhs) {
-    return _stricmp(lhs.c_str(), rhs.c_str()) < 0;
-  }
-};
 
 template<typename T, typename Comparator>
-const T& find_max(const std::vector<T>& v, Comparator cmp) {
+const T& find_max(const std::vector<T>& v, Comparator is_less_than) {
   int max_index = 0;
 
   for(size_t i = 1; i < v.size(); ++i) {
-    if(cmp.is_less_than(v[max_index], v[i])) {
+    if(is_less_than(v[max_index], v[i])) {
       max_index = i;
     }
   }
 
   return v[max_index];
 }
+
+template<typename T> // using default ordering
+const T& find_max(const std::vector<T>& v) {
+  return find_max(v, std::less<T>());
+}
+
+
+class case_sensitive_cmp {
+public:
+  bool operator()(const std::string& lhs, const std::string& rhs) {
+    return _stricmp(lhs.c_str(), rhs.c_str()) < 0;
+  }
+};
 
 int main() {
 
@@ -29,8 +37,8 @@ int main() {
   v[1] = "alligator";
   v[2] = "crocodile";
 
-  std::cout << find_max(v, case_insensitive_cmp());
-
+  std::cout << find_max(v, case_sensitive_cmp()) << std::endl;
+  std::cout << find_max(v);
  
   std::cin.get();
   return 0;
