b3e19b7a57f724d1829f1ce47abb233d2a1e4ef9
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-01-23 11:59:03 +0400

    word puzzle

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,44 +1,173 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iterator>
+#include <string>
 
 
 int main() {
 
-  std::vector<int> vi;
+  std::vector<std::vector<char> > board { { 't', 'h', 'i', 's' },
+                                          { 'w', 'a', 't', 's' },
+                                          { 'o', 'a', 'h', 'g' },
+                                          { 'f', 'g', 'o', 't' } };
 
-  int n;
-  std::cout << "enter n: ";
-  std::cin >> n;
-  n = (int)n;
-  
-  int k = n / 2;
+  std::vector<std::string> words = { "this", "his", "is", "hi", "at", "hat", "go",
+                                     "that", "two", "it", "fat", "to", "got", "oh" };
 
-  std::cout << '\n';
+  std::vector<std::string> guessed_words;
 
-  int item;
-  for(int i = 0; i < k; ++i) {
-    std::cout << "enter item " << i + 1 << ": ";
-    std::cin >> item;
-    vi.push_back(item);
-  }
-  std::sort(vi.begin(), vi.end(), [](int a, int b) {return a > b; });
-
-  for(int i = 0; i < k; ++i) {
-    std::cout << "enter another item: ";
-    std::cin >> item;
-    if(item > vi[k - 1]) {
-      vi.erase(vi.begin() + k-1);
-      vi.push_back(item);
-      std::sort(vi.begin(), vi.end(), [](int a, int b) {return a > b; });
+  // ->
+  for(size_t row = 0; row < board.size(); ++row) {
+    for(size_t letter = 0; letter < board.size() - 1; ++letter) {
+      std::string word(1, board[row][letter]);
+      for(size_t other = letter + 1; other < board.size(); ++other) {
+        word.append(1, board[row][other]);
+        if(std::find(words.begin(), words.end(), word) != words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
     }
   }
 
-  std::cout << vi[k-1];
+  // |
+  // v
+  for(size_t row = 0; row < board.size(); ++row) {
+    for(size_t letter = 0; letter < board.size() - 1; ++letter) {
+      std::string word(1, board[letter][row]);
+      for(size_t other = letter + 1; other < board.size(); ++other) {
+        word.append(1, board[other][row]);
+        if(std::find(words.begin(), words.end(), word) != words.end() &&
+           std::find(guessed_words.begin(), guessed_words.end(), word) == guessed_words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
+
+  // <-
+  for(size_t row = board.size() - 1; row > 0; --row) {
+    for(size_t letter = board.size() - 1; letter > 0; --letter) {
+      std::string word(1, board[row][letter]);
+      for(size_t other = letter - 1; other > 0; --other) {
+        word.append(1, board[row][other]);
+        if(std::find(words.begin(), words.end(), word) != words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
+
+  // ^
+  // |
+  for(size_t row = board.size() - 1; row > 0; --row) {
+    for(size_t letter = board.size() - 1; letter > 0; --letter) {
+      std::string word(1, board[letter][row]);
+      for(size_t other = letter - 1; other > 0; --other) {
+        word.append(1, board[other][row]);
+        if(std::find(words.begin(), words.end(), word) != words.end() &&
+           std::find(guessed_words.begin(), guessed_words.end(), word) == guessed_words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
 
 
-  std::cin.get();
+  // \
+  //  >
+  for(size_t row = 0; row < board.size() - 1; ++row) {
+    for(size_t col = 0; col < board.size() - 1; ++col) {
+      std::string word(1, board[row][col]);
+
+      int new_row = row;
+      int new_col = col;
+
+      for(int i = col + 1; i < board.size() - row; ++i) {
+        ++new_row;
+        ++new_col;
+        word.append(1, board[new_row][new_col]);
+        if(std::find(words.begin(), words.end(), word) != words.end() &&
+           std::find(guessed_words.begin(), guessed_words.end(), word) == guessed_words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
+
+
+  //  /
+  // <
+  for(size_t row = 0; row < board.size() - 2; ++row) {
+    for(size_t col = board.size() - 1; col > 0; --col) {
+      std::string word(1, board[row][col]);
+
+      int new_row = row;
+      int new_col = col;
+
+      for(int i = 0; i < col - row; ++i) {
+        ++new_row;
+        --new_col;
+        word.append(1, board[new_row][new_col]);
+        if(std::find(words.begin(), words.end(), word) != words.end() &&
+           std::find(guessed_words.begin(), guessed_words.end(), word) == guessed_words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
+
+
+  //  ^
+  // /
+  for(size_t row = board.size() - 1; row > 0; --row) {
+    for(size_t col = 0; col < board.size() - 1; ++col) {
+      std::string word(1, board[row][col]);
+
+      size_t new_row = row;
+      size_t new_col = col;
+
+      for(size_t i = 0; i < abs(static_cast<long>(row - col)); ++i) {
+        --new_row;
+        ++new_col;
+        word.append(1, board[new_row][new_col]);
+        if(std::find(words.begin(), words.end(), word) != words.end() &&
+           std::find(guessed_words.begin(), guessed_words.end(), word) == guessed_words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
+
+
+  // ^
+  /*  \ */
+  for(size_t row = board.size() - 1; row > 0; --row) {
+    for(size_t col = board.size() - 1; col > 0; --col) {
+      std::string word(1, board[row][col]);
+
+      size_t new_row = row;
+      size_t new_col = col;
+
+      for(size_t i = 0; i < std::min(row, col); ++i) {
+        --new_row;
+        --new_col;
+        word.append(1, board[new_row][new_col]);
+        if(std::find(words.begin(), words.end(), word) != words.end() &&
+           std::find(guessed_words.begin(), guessed_words.end(), word) == guessed_words.end()) {
+          guessed_words.push_back(word);
+          std::cout << word << std::endl;
+        }
+      }
+    }
+  }
+
   std::cin.get();
   return 0;
 }
