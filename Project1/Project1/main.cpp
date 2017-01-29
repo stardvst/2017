fe5d752c1b2253982cbfd528458738ed40d9809a
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-01-29 22:37:33 +0400

    josephus problem using linked list

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,110 +1,48 @@
 #include <iostream>
-#include <algorithm>
-#include <functional>
 #include <list>
 
-std::list<int> get_intersection(const std::list<int>& l1, const std::list<int>& l2) {
-  std::list<int> result;
-
-  for(std::list<int>::const_iterator l1it = l1.begin(); l1it != l1.end(); ++l1it) {
-    std::list<int>::const_iterator l2it = l2.begin();
-    while(l2it != l2.end() && *l2it != *l1it) {
-      ++l2it;
-    }
-    if(l2it != l2.end()) {
-      if(*l1it == *l2it && std::find(result.begin(), result.end(), *l1it) == result.end()) {
-        result.push_back(*l1it);
+int josephus_problem(std::list<int>& people, int m) {
+  std::list<int>::iterator person = people.begin();
+  while(people.size() > 1) {
+    for(int i = 0; i < m; ++i) {
+      if(*person == people.back()) {
+        person = people.begin();
+      } else {
+        ++person;
       }
     }
-  }
-
-  return result;
-}
-
-template<typename C>
-std::list<int> get_union(const std::list<int>& l1, const std::list<int>& l2, C cmp) {
-  std::list<int> result;
-
-  std::list<int>::const_iterator l1it = l1.begin();
-  std::list<int>::const_iterator l2it = l2.begin();
-
-  while(l1it != l1.end() && l2it != l2.end()) {
-    if(cmp(*l1it, *l2it)) {
-      result.push_back(*l1it);
-      ++l1it;
-    } else if(cmp(*l2it, *l1it)) {
-      result.push_back(*l2it);
-      ++l2it;
+    if(*person == people.back()) {
+      std::cout << "deleting " << *person << '\n';
+      people.erase(person);
+      person = people.begin();
     } else {
-      result.push_back(*l1it);
-      ++l1it;
-      ++l2it;
+      std::cout << "deleting " << *person << '\n';
+      person = people.erase(person);
     }
   }
-
-  while(l1it != l1.end()) {
-    if(std::find(result.begin(), result.end(), *l1it) == result.end()) { result.push_back(*l1it); }
-    ++l1it;
-  }
-  while(l2it != l2.end()) {
-    if(std::find(result.begin(), result.end(), *l2it) == result.end()) { result.push_back(*l2it); }
-    ++l2it;
-  }
-
-  return result;
+  return *(people.begin());
 }
 
-
 int main() {
+  
+  int n;
+  std::cout << "n = ";
+  std::cin >> n;
 
-  std::list<int> l1;
-  std::list<int> l2;
-
-  l1.push_back(4);
-  l1.push_back(5);
-  l1.push_back(2);
-  l1.push_back(-5);
-  l1.push_back(3);
-  l1.push_back(-1);
-  l1.push_back(0);
-  l1.push_back(7);
-
-  l2.push_back(5);
-  l2.push_back(0);
-  l2.push_back(8);
-  l2.push_back(6);
-  l2.push_back(7);
-  l2.push_back(3);
-  l2.push_back(-10);
-
-  l1.sort(std::greater<int>());
-  std::cout << "L1\t";
-  for(std::list<int>::const_iterator it = l1.begin(); it != l1.end(); ++it) {
-    std::cout << *it << " -> ";
+  std::list<int> people;
+  for(int i = 1; i <= n; ++i) {
+    people.push_back(i);
   }
-  std::cout << "NULL\n\n";
 
-  l2.sort(std::greater<int>());
-  std::cout << "L2\t";
-  for(std::list<int>::const_iterator it = l2.begin(); it != l2.end(); ++it) {
-    std::cout << *it << " -> ";
-  }
-  std::cout << "NULL\n\n";
+  int m;
+  std::cout << "m = ";
+  std::cin >> m;
+  std::cout << '\n';
 
-  std::list<int> result = get_intersection(l1, l2);
-  std::cout << "L1&L2\t";
-  for(std::list<int>::const_iterator it = result.begin(); it != result.end(); ++it) {
-    std::cout << *it << " -> ";
-  }
-  std::cout << "NULL\n\n";
+  std::cout << "\nwinner: " << josephus_problem(people, m);
 
-  result = get_union(l1, l2, std::greater<int>());
-  std::cout << "L1|L2\t";
-  for(std::list<int>::const_iterator it = result.begin(); it != result.end(); ++it) {
-    std::cout << *it << " -> ";
-  }
-  std::cout << "NULL";
 
+  std::cin.get();
   std::cin.get();
   return 0;
 }
