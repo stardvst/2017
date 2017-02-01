096b17183704477ebf90ab12c2fbfd9c7ee8583b
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-02-01 12:54:53 +0400

    evaluate postfix expression (+,-,*,/)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,131 +2,39 @@
 #include <string>
 #include <stack>
 
-bool check_match(std::stack<char>& symbols, char symbol) {
-  if(symbols.size() && 
-     (symbol == ')' && symbols.top() == '(') ||
-     (symbol == ']' && symbols.top() == '[') ||
-     (symbol == '}' && symbols.top() == '{')) {
-    symbols.pop();
-    return true;
-  }
-  return false;
-}
 
-bool balancing_symbols_pascal(const std::string& input) {
+int evaluate(const std::string& expression) {
 
-  std::stack<char> symbols;
-  bool balanced = true;
+  std::stack<int> operands;
+  int result = 0;
+  
+  for(size_t i = 0; i < expression.length(); ++i) {
+    if(expression[i] == ' ') {
+      continue;
+    } else if(isdigit(expression[i])) {
+      operands.push(expression[i] - '0'); // convert ascii to int
+    } else {
+      int op1 = operands.top();
+      operands.pop();
+      int op2 = operands.top();
+      operands.pop();
 
-  for(size_t i = 0; i < input.length(); ++i) {
-    switch(input[i]) {
-    case '(': 
-    case '[': 
-    case '{': 
-      symbols.push(input[i]); break;
-    case 'b':
-      if(i != input.length() - 4 &&
-         input[i + 2] == 'g' &&
-         input[i + 3] == 'i' &&
-         input[i + 4] == 'n') {
-        symbols.push('b');
-        i += 3; // advaance i to point to the symbol after "begin"
+      switch(expression[i]) {
+        case '+': operands.push(op2 + op1); break;
+        case '-': operands.push(op2 - op1); break;
+        case '*': operands.push(op2 * op1); break;
+        case '/': operands.push(op2 / op1); break;
       }
-      break;
-    case ')':
-    case ']':
-    case '}':
-      balanced = check_match(symbols, input[i]);
-      break;
-    case 'e':
-      if(i != input.length() - 2 &&
-         input[i + 1] == 'n'&&
-         input[i + 2] == 'd') {
-        symbols.size() && symbols.top() == 'b' ? symbols.pop() : balanced = false;
-        ++i; // advaance i to point to the symbol after "end"
-      }
-      break;
-    default:
-      break;
     }
   }
 
-  if(!symbols.empty()) {
-    balanced = false;
-  }
-
-  return balanced;
+  return operands.top();
 }
 
-bool balancing_symbols_cpp(const std::string& input) {
-
-  std::stack<char> symbols;
-  bool balanced = true;
-
-  for(size_t i = 0; i < input.length(); ++i) {
-    switch(input[i]) {
-    case '(':
-    case '[':
-    case '{':
-      symbols.push(input[i]); break;
-    case '/':
-      if(i != input.length() - 1 &&
-         input[i + 1] == '*') {
-        symbols.push('/');
-        ++i; // advaance i to point to the symbol after "/*"
-      }
-      break;
-    case ')':
-    case ']':
-    case '}':
-      balanced = check_match(symbols, input[i]);
-      break;
-    case '*':
-      if(i != input.length() - 1 &&
-         input[i + 1] == '/') {
-        symbols.size() && symbols.top() == '/' ? symbols.pop() : balanced = false;
-        ++i; // advaance i to point to the symbol after "end"
-      }
-      break;
-    default:
-      break;
-    }
-  }
-
-  if(!symbols.empty()) {
-    balanced = false;
-  }
-
-  return balanced;
-}
 
 int main() {
 
-  std::string input;
-
-  /* check for pascal */
-  std::cout << "enter a code snippet in pascal:\n\n";
-  
-  std::string line = " ";
-  while(line.length() != 0) {
-    std::getline(std::cin, line);
-    input += line;
-  }
-
-  std::cout << "the code in pascal is balanced: "
-    << std::boolalpha << balancing_symbols_pascal(input) << "\n\n\n\n";
-
-  /* check for c++ */
-  std::cout << "enter a code snippet in c++:\n\n";
-
-  line = " ";
-  while(line.length() != 0) {
-    std::getline(std::cin, line);
-    input += line;
-  }
-
-  std::cout << "the code in c++ is balanced: "
-    << std::boolalpha << balancing_symbols_cpp(input);
+  std::cout << evaluate("231*+9-");
 
   std::cin.get();
   return 0;
