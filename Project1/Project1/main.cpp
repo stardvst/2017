063698c2f16e89590551a691d6ddcc6e7e77a2c0
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-02-03 20:50:00 +0400

    infix -> postfix: (,),+,-,*,/,^ postfix -> infix without redundant parentheses

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 #include <stack>
 
+double evaluate(const std::string& expression) {
 
-int evaluate(const std::string& expression) {
-
-  std::stack<int> operands;
+  std::stack<double> operands;
   int result = 0;
   
   for(size_t i = 0; i < expression.length(); ++i) {
-    if(expression[i] == ' ') {
+    if(isspace(expression[i])) {
       continue;
     } else if(isdigit(expression[i])) {
       operands.push(expression[i] - '0'); // convert ascii to int
     } else {
-      int op1 = operands.top();
+      double op1 = operands.top();
       operands.pop();
-      int op2 = operands.top();
+      double op2 = operands.top();
       operands.pop();
 
       switch(expression[i]) {
@@ -24,6 +24,7 @@ int evaluate(const std::string& expression) {
         case '-': operands.push(op2 - op1); break;
         case '*': operands.push(op2 * op1); break;
         case '/': operands.push(op2 / op1); break;
+        case '^': operands.push(pow(op2, op1)); break;
       }
     }
   }
@@ -32,9 +33,136 @@ int evaluate(const std::string& expression) {
 }
 
 
+std::string to_postfix(const std::string& infix) {
+
+  std::stack<char> operators;
+  std::string result;
+
+  for(size_t i = 0; i < infix.length(); ++i) {
+    if(isspace(infix[i])) {
+      continue;
+    } else if(isdigit(infix[i])) {
+      result += infix[i];
+    } else {
+      switch(infix[i]) {
+        case '+':
+        case '-':
+          if(!operators.empty() && (operators.top() == '+' || operators.top() == '-' ||
+                                  operators.top() == '*' || operators.top() == '/')) {
+            result += operators.top();
+            operators.pop();
+            if(!operators.empty() && (operators.top() == '+' || operators.top() == '-')) {
+              result += operators.top();
+               operators.pop();
+            }
+          }
+          operators.push(infix[i]);
+          break;
+        case '*':
+        case '/':
+          if(!operators.empty() && (operators.top() == '*' || operators.top() == '/')) {
+            result += operators.top();
+            operators.pop();
+          }
+          operators.push(infix[i]);
+          break;
+        case '^':
+          if(!operators.empty() && (operators.top() == '^')) {
+            result += '^';
+          } else {
+            operators.push('^');
+          }
+          break;
+        case '(': 
+          operators.push('(');
+          break;
+        case ')':
+          while(operators.top() != '(') {
+            result += operators.top();
+            operators.pop();
+          }
+          operators.pop();
+          break;
+      }
+    }
+  }
+  
+  while(!operators.empty()) {
+    result += operators.top();
+    operators.pop();
+  }
+  return result;
+}
+
+
+struct Term {
+  std::string expr;
+  char oper;
+  Term(std::string e, char o) : expr(e), oper(o) {}
+};
+
+std::string to_infix(const std::string& postfix) {
+
+  std::stack<Term> operands;
+
+  for(size_t i = 0; i < postfix.length(); ++i) {
+    char left_prev_operator;
+    char right_prev_operator;
+
+    if(isspace(postfix[i])) {
+      continue;
+    } else if(isdigit(postfix[i])) {
+      operands.push(Term(std::to_string(postfix[i] - '0'), '\0'));
+    } else {
+      Term op1 = operands.top();
+      operands.pop();
+      Term op2 = operands.top();
+      operands.pop();
+      
+      std::string tmp;
+      std::string right_expr = op1.expr;
+      std::string left_expr = op2.expr;
+
+      switch(postfix[i]) {
+        case '*':
+        case '/':
+          if(op1.oper == '+' || op1.oper == '-') {
+            right_expr = "(" + op1.expr + ")";
+          }
+          if(op2.oper == '+' || op2.oper == '-') {
+            left_expr = "(" + op2.expr + ")";
+          }
+          tmp = left_expr + postfix[i] + right_expr;
+          break;
+        case '^':
+          if(op1.oper == '*' || op1.oper == '/' || op1.oper == '+' || op1.oper == '-') {
+            right_expr = "(" + op1.expr + ")";
+          }
+          if(op2.oper == '*' || op2.oper == '/' || op2.oper == '+' || op2.oper == '-') {
+            left_expr = "(" + op2.expr + ")";
+          }
+          tmp = left_expr + postfix[i] + right_expr;
+          break;
+        default: // +, -
+          tmp = op2.expr + postfix[i] + op1.expr;
+          break;
+      }
+
+      operands.push(Term(tmp, postfix[i]));
+    }
+  }
+
+  return operands.top().expr;
+}
+
+
 int main() {
 
-  std::cout << evaluate("231*+9-");
+  std::cout << evaluate(to_postfix("5+4*7+(5*3+4)*3")) << '\n'
+    << evaluate(to_postfix("5*3^(4-2)")) << '\n'
+    << to_infix(to_postfix("5+4*7+(5*3+4)*(4+5)")) << '\n'
+    << to_infix(to_postfix("5*3^(4-2)")) << '\n'
+    << to_infix(to_postfix("5-4-2"));
 
   std::cin.get();
   return 0;
