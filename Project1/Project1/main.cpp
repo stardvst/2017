314c32e6ec55c23d323e4d8604246a7cb5b86687
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-02-28 19:15:08 +0400

    algo - number of chars to remove from two strings to make them anagrams

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,85 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 
-size_t merge_and_count(std::vector<int>& arr, 
-                       std::vector<int>& tmp,
-                       size_t low, size_t mid, size_t high) {
-  size_t i = low;
-  size_t j = mid + 1;
-  size_t k = low;
+int chars(const std::string& str1, const std::string& str2) {
+  std::vector<int> occurences(26);
 
-  size_t count = 0;
-
-  while(i <= mid && j <= high) {
-    if(arr[i] <= arr[j]) {
-      tmp[k++] = arr[i++];
-    } else {
-      tmp[k++] = arr[j++];
-      count += mid + 1 - i;
-    }
+  for(size_t i = 0; i < str1.length(); ++i) {
+    occurences[str1[i] - 'a']++;
   }
 
-  while(i <= mid) {
-    tmp[k++] = arr[i++];
-  }
-  while(j <= high) {
-    tmp[k++] = arr[j++];
+  for(size_t i = 0; i < str2.length(); ++i) {
+    occurences[str2[i] - 'a']--;
   }
 
-  for(size_t t = low; t <= high; ++t) {
-    arr[t] = tmp[t];
+  int sum = 0;
+  for(std::vector<int>::const_iterator i = occurences.begin();
+      i != occurences.end(); ++i) {
+    sum += std::abs(*i);
   }
 
-  return count;
+  return sum;
 }
-
-size_t sort_and_count(std::vector<int>& arr, 
-                      std::vector<int>& tmp, 
-                      size_t low, size_t high) {
-  if(low < high) {
-    size_t mid = low + (high - low) / 2;
-
-    size_t X = sort_and_count(arr, tmp, low, mid);
-    size_t Y = sort_and_count(arr, tmp, mid + 1, high);
-    size_t Z = merge_and_count(arr, tmp, low, mid, high);
-
-    return X + Y + Z;
-  }
-
-  return 0;
-}
-
-size_t sort_and_count(std::vector<int>& arr) {
-  std::vector<int> tmp(arr.size());
-  return sort_and_count(arr, tmp, 0, arr.size() - 1);
-}
-
 
 int main() {
 
-  std::vector<int> arr;
+  std::string str1;
+  std::cout << "enter str1: ";
+  std::cin >> str1;
 
-  int number = 0;
-  while(number <= 0) {
-    std::cout << "enter number of elements: ";
-    std::cin >> number;
-  }
+  std::string str2;
+  std::cout << "enter str2: ";
+  std::cin >> str2;
 
-  int current;
-  for(int i = 0; i < number; ++i) {
-    std::cout << "arr[0] = ";
-    std::cin >> current;
-    arr.push_back(current);
-  }
-
-  std::cout << "\narr: ";
-  for(std::vector<int>::const_iterator it = arr.begin(); it != arr.end(); ++it) {
-    std::cout << *it << " ";
-  }
-  
-  std::cout << "\n\nnumber of inversions: " << sort_and_count(arr);
-
+  std::cout << '\n' << chars(str1, str2) << " chars to remove to make \"" <<
+    str1 << "\" and \"" << str2 << "\" anagrams";
 
   std::cin.get();
   std::cin.get();
