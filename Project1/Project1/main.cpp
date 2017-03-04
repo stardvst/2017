ec45956487553ea27b6423bee7aebc38c4a30858
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-03-04 15:04:31 +0400

    recursive insertion sort, binary search (recursive, iterative)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,20 +2,52 @@
 #include <vector>
 
 
-void selection_sort(std::vector<int>& A) {
-  for(int i = 0; i < A.size() - 1; ++i) {
-    int min = i;
+void insertion_sort(std::vector<int>& A, int index) {
+  if(index == 0) return;
 
-    for(int j = i + 1; j < A.size(); ++j) {
-      if(A[j] < A[min]) {
-        min = j;
-      }
-    }
+  insertion_sort(A, index - 1);
 
-    int tmp = A[i];
-    A[i] = A[min];
-    A[min] = tmp;
+  int key = A[index];
+  while(index - 1 >= 0 && A[index - 1] > key) {
+    A[index] = A[index - 1];
+    index = index - 1;
   }
+  A[index] = key;
+}
+
+
+int binary_search(const std::vector<int>& A, int key, int start, int end) {
+  if(start <= end) {
+    int mid = (start + end) / 2;
+    if(key == A[mid]) return mid;
+
+    if(key > A[mid]) {
+      return binary_search(A, key, mid + 1, end);
+    } else {
+      return binary_search(A, key, start, mid - 1);
+    }
+  }
+
+  return -1;
+}
+
+int binary_search(const std::vector<int>& A, int key) {
+  int start = 0;
+  int end = static_cast<int>(A.size());
+
+  while(start <= end) {
+    int mid = (start + end) / 2;
+
+    if(key == A[mid]) return mid;
+
+    if(key > A[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
 
@@ -23,19 +55,30 @@ int main() {
 
   std::vector<int> A;
 
-  for(int i = 0; i < 7; ++i) {
+  int number = 0;
+  while(number <= 0) {
+    std::cout << "enter the numbers of the array: ";
+    std::cin >> number;
+  }
+  for(int i = 0; i < number; ++i) {
     int current;
     std::cout << "A[" << i << "] = ";
     std::cin >> current;
     A.push_back(current);
   }
 
-  selection_sort(A);
+  insertion_sort(A, number - 1);
 
   std::cout << "\nsorted array: ";
-  for(int i = 0; i < 7; ++i) {
+  for(int i = 0; i < number; ++i) {
     std::cout << A[i] << ' ';
   }
+
+  int key;
+  std::cout << "\n\nenter a key to search for: ";
+  std::cin >> key;
+  std::cout << key << " is at index " << binary_search(A, key, 0, number - 1) << 
+    " (recursive), " << binary_search(A, key) << " (iterative)";
 
   std::cin.get();
   std::cin.get();
