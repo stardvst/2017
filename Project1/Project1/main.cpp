9f02143dee2a2848e1c3987b65987ebf23868e11
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-03-14 16:51:10 +0400

    quicksort (pivot = A[high])

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
-#include <string>
-#include "postfix.hpp"
+#include <vector>
+
+
+int partition(std::vector<int>& A, int low, int high) {
+  int pivot = A[high];
+  int i = low - 1;
+  for(int j = low; j < high; ++j) {
+    if(A[j] <= pivot) {
+      ++i;
+      std::swap(A[i], A[j]);
+    }
+  }
+  std::swap(A[i + 1], A[high]);
+  return i + 1;
+}
+
+
+void quicksort(std::vector<int>& A, int low, int high) {
+  if(low < high) {
+    int q = partition(A, low, high);
+    quicksort(A, low, q - 1);
+    quicksort(A, q + 1, high);
+  }
+}
 
 
 int main() {
 
-  std::string infix;
-  std::cout << "enter infix expression: ";
-  std::getline(std::cin, infix);
+  std::vector<int> A;
 
-  std::string postfix = to_postfix(infix);
-  std::cout << "\ncorresponding postfix expression is: " << postfix << "\n\n";
+  for(int i = 0; i < 6; ++i) {
+    int current;
+    std::cout << "A[" << i << "] = ";
+    std::cin >> current;
+    A.push_back(current);
+  }
 
-  evaluate(postfix);
+  quicksort(A, 0, static_cast<int>(A.size() - 1));
 
+  std::cout << "\nsorted array: ";
+  for(int i = 0; i < 6; ++i) {
+    std::cout << A[i] << ' ';
+  }
+
+  std::cin.get();
   std::cin.get();
   return 0;
 }
