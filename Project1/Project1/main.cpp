c629922b9a5bfd89b0cf2ab18f593cd6a2a5c0ed
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-03-28 19:09:24 +0400

    algo - # of symbols to remove to make strings anagrams

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
 
 
 int main() {
 
-  int num;
-  std::cout << "num: ";
-  std::cin >> num;
+  std::string str1;
+  std::string str2;
   
-  
-  short t = 0;
-  while(num) {
-    ++t;
-    num &= num - 1; // remove last 1 bit
+  std::cout << "str1: ";
+  std::cin >> str1;
+  std::cout << "str2: ";
+  std::cin >> str2;
+
+
+  std::vector<int> count(26);
+
+  for(size_t i = 0; i < str1.length(); ++i) {
+    ++count[str1[i] - 'a'];
   }
 
-  std::cout << "1's: " << t << std::endl;
+  for(size_t i = 0; i < str2.length(); ++i) {
+    --count[str2[i] - 'a'];
+  }
 
+  int symbol_count = 0;
+  for(size_t i = 0; i < count.size(); ++i) {
+    symbol_count += std::abs(count[i]);
+  }
+
+  std::cout << "remove " << symbol_count << " symbols\n";
 
   std::cin.get();
   std::cin.get();
