01cc3353eba5267a151ed194dfff2da397be691a
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-01 20:14:12 +0400

    static vars are init-ed to 0 (bool - false)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,62 +1,24 @@
 #include <iostream>
-#include <cstddef>
 
 
+template<typename T, T t = T()>
 class A {
-public:
-  A() : x(0) {}
-
-  static ptrdiff_t member_offset(const A& a) {
-    const char *p = reinterpret_cast<const char*>(&a);
-    const char *q = reinterpret_cast<const char*>(&a.x);
-    return q - p;
-  }
 private:
-  int x;
-}; 
+  template<bool b>
+  struct B {
+    static const int n = b ? 1 : 0;
+  };
 
-class B : public A {
 public:
-  B() : x('a') {}
-
-  static int n;
-
-  static ptrdiff_t member_offset(const B& b) {
-    const char *p = reinterpret_cast<const char*>(&b);
-    const char *q = reinterpret_cast<const char*>(&b.x);
-    return q - p;
-  }
-private:
-  char x;
+  static const int value = B<(t > T())>::n - B<(t < T())>::n;
 };
 
-int B::n = 1;
-
-
-class C {
-public:
-  C() : x(0) {}
-  virtual ~C() {}
-
-  static ptrdiff_t member_offset(const C& c) {
-    const char *p = reinterpret_cast<const char*>(&c);
-    const char *q = reinterpret_cast<const char*>(&c.x);
-    return q - p;
-  }
-private:
-  int x;
-};
 
 int main() {
 
-  A a;
-  B b;
-  C c;
-
-  std::cout << ((A::member_offset(a) == 0) ? 0 : 1);
-  std::cout << ((B::member_offset(b) == 0) ? 0 : 2);
-  std::cout << ((A::member_offset(b) == 0) ? 0 : 3);
-  std::cout << ((C::member_offset(c) == 0) ? 0 : 4);
+  std::cout << A<int, -9>::value;
+  std::cout << A<bool, true>::value;
+  std::cout << A<char>::value;
 
   std::cin.get();
   return 0;
