e6bed265a3a919c4d6f146b51877b2eed81a62a0
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-01 20:03:57 +0400

    data member offset from the object itself (interesting one!)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
-#include <algorithm>
-#include <functional>
-#include <iterator>
-#include <list>
+#include <cstddef>
 
+
+class A {
+public:
+  A() : x(0) {}
+
+  static ptrdiff_t member_offset(const A& a) {
+    const char *p = reinterpret_cast<const char*>(&a);
+    const char *q = reinterpret_cast<const char*>(&a.x);
+    return q - p;
+  }
+private:
+  int x;
+}; 
+
+class B : public A {
+public:
+  B() : x('a') {}
+
+  static int n;
+
+  static ptrdiff_t member_offset(const B& b) {
+    const char *p = reinterpret_cast<const char*>(&b);
+    const char *q = reinterpret_cast<const char*>(&b.x);
+    return q - p;
+  }
+private:
+  char x;
+};
+
+int B::n = 1;
+
+
+class C {
+public:
+  C() : x(0) {}
+  virtual ~C() {}
+
+  static ptrdiff_t member_offset(const C& c) {
+    const char *p = reinterpret_cast<const char*>(&c);
+    const char *q = reinterpret_cast<const char*>(&c.x);
+    return q - p;
+  }
+private:
+  int x;
+};
 
 int main() {
 
-  typedef std::list<int> L;
-  L l(5);
+  A a;
+  B b;
+  C c;
 
-  typedef L::const_iterator CI;
-  CI cb = l.begin(), ce = l.end();
-
-  typedef L::iterator I;
-  I b = l.begin();
-
-  std::transform(cb, --ce, // input range
-    ++b, // output range
-    std::bind2nd(std::plus<CI::value_type>(), 1) // what to do
-  );
-
-  std::copy(l.begin(), l.end(),
-            std::ostream_iterator<CI::value_type>(std::cout));
+  std::cout << ((A::member_offset(a) == 0) ? 0 : 1);
+  std::cout << ((B::member_offset(b) == 0) ? 0 : 2);
+  std::cout << ((A::member_offset(b) == 0) ? 0 : 3);
+  std::cout << ((C::member_offset(c) == 0) ? 0 : 4);
 
   std::cin.get();
   return 0;
