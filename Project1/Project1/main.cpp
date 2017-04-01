9db9c84e5ac7175a90e07779bd65ac61be6b282e
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-01 19:14:25 +0400

    member data of other class

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -4,37 +4,25 @@
 
 class A {
 public:
-  A(int nn = 0) : n(nn) {}
-
-  virtual int value() const { return n; }
-  virtual ~A() {}
+  A(int nn = 2) : n(nn) {}
+  ~A() { std::cout << n; }
 protected:
   int n;
 };
 
 class B : public A {
 public:
-  B(int nn = 0) : A(nn) {}
-  virtual int value() const { return n + 1; }
+  B(int nn) : x(n + 1), a(nn) {}
+  ~B() { std::cout << n; --n; }
+private:
+  A x;
+  A a;
 };
 
 
 int main() {
 
-  const A a(1);
-  const B b(3);
-  const A* x[2] = { &a, &b }; 
-
-  typedef std::vector<A> V; 
-  V y;
-  y.push_back(a);
-  y.push_back(b);
-
-  V::const_iterator i = y.begin();
-  std::cout << x[0]->value(); 
-  std::cout << x[1]->value(); // x is container of ptrs => B's value() is called
-  std::cout << i->value();
-  std::cout << (i + 1)->value(); // y is container of obs => A's value() is called
+  { B b(5); }
 
   std::cin.get();
   return 0;
