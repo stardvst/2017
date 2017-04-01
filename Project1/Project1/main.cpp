8508c2a49559216b7901ea97db3d61cac4e391df
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-01 18:24:05 +0400

    copy ctor calls

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 
 
-template<typename T>
-void kill(T*& t) {
-  delete t;
-  t = 0;
-}
+class A {
+public:
+  explicit A(int nn = 0) : n(nn) {}
+  A(const A& a) : n(a.n) {
+    ++copy_ctor_calls;
+  }
+public:
+  static int copy_ctor_calls;
+private:
+  int n;
+};
 
-struct A {};
-
+int A::copy_ctor_calls = 0;
+A f(const A& a) { return a; }
+A g(const A a) { return a; }
 
 int main() {
 
-  A* ptr = new A();
-  kill(ptr);
-  kill(ptr);
+  A a;
+  A b = a, c(a);
+  std::cout << A::copy_ctor_calls;
 
+  b = g(c);
+  std::cout << A::copy_ctor_calls;
+
+  const A& d = f(c);
+  std::cout << A::copy_ctor_calls;
+  d;
 
    std::cin.get();
   return 0;
-
-  // ~A()
 }
