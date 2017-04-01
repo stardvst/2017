2c95a603f6d1b277ddfecaebcd0846c9926999ff
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-01 14:42:41 +0400

    function overloads

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,30 +1,15 @@
 #include <iostream>
 
 
-struct Exc1 {
-  void what() { std::cout << "Exc1" << std::endl; }
-};
-
-struct Exc2 : public Exc1 {
-  void what() { std::cout << "Exc2" << std::endl; }
-};
+void f(int) { std::cout << "int" << std::endl; }
+void f(unsigned short) { std::cout << "unsigned short" << std::endl; }
+void f(unsigned int) { std::cout << "unsigned int" << std::endl; }
 
 
 int main() {
 
-  try {
-    throw Exc2();
-  }
-  /*catch(Exc1& e) {
-    std::cout << "1st catch: "; // Exc2& is caught by base class
-    e.what();
-  }*/
-  catch(Exc2& e) {
-    std::cout << "2nd catch: ";
-    e.what();
-  } catch(...) {
-    std::cout << "unknown exception\n";
-  }
+  short c = 5;
+  f(c);
 
   std::cin.get();
   return 0;
