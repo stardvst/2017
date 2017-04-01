0639031e4fa423cff70d6939601a1dfcee9e8f57
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-01 12:41:42 +0400

    can't understand this :( int(*p)[10]

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,36 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <typeinfo>
 
 
 int main() {
 
-  std::string str1;
-  std::string str2;
-  
-  std::cout << "str1: ";
-  std::cin >> str1;
-  std::cout << "str2: ";
-  std::cin >> str2;
+  int(*p)[10] = { 0 };
 
-
-  std::vector<int> count(26);
-
-  for(size_t i = 0; i < str1.length(); ++i) {
-    ++count[str1[i] - 'a'];
-  }
-
-  for(size_t i = 0; i < str2.length(); ++i) {
-    --count[str2[i] - 'a'];
-  }
-
-  int symbol_count = 0;
-  for(size_t i = 0; i < count.size(); ++i) {
-    symbol_count += std::abs(count[i]);
-  }
-
-  std::cout << "remove " << symbol_count << " symbols\n";
+  std::cout << typeid(p).name() << std::endl;
+  std::cout << (size_t)(p + 1) << std::endl;
+  std::cout << (size_t)p << std::endl;
 
   std::cin.get();
-  std::cin.get();
+  return 0;
 }
