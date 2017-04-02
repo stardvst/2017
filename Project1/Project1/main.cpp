eeb3f4682edd87d9d867a581abac373123d7aecd
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-02 21:30:02 +0400

    default arguments of BASE CLASS are used

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,41 +1,21 @@
 #include <iostream>
 
 
-class A {
-public:
-  A(int nn = 0) : n(nn) {}
-  
-  A(const A& a) : n(a.n) { ++copy_ctor_calls; }
-  ~A() { ++dtor_calls; }
-
-  static int copy_ctor_calls;
-  static int dtor_calls;
-private:
-  int n;
+struct A {
+  virtual int shift(int n = 2) const { return n << 2; }
 };
 
-int A::copy_ctor_calls = 0;
-int A::dtor_calls = 0;
+struct B : A {
+  int shift(int n = 3) const { return n << 3; }
+};
 
 
 int main() {
 
-  A* p = 0;
+  const B b;
+  const A* a = &b;
 
-  {
-    const A a = 2;
-    p = new A[3];
-    p[0] = a;
-  }
-
-  std::cout << A::copy_ctor_calls << A::dtor_calls;
-
-  p[1] = A(1);
-  p[2] = 2;
-
-  delete[] p;
-
-  std::cout << A::copy_ctor_calls << A::dtor_calls;
+  std::cout << a->shift();
 
   std::cin.get();
   return 0;
