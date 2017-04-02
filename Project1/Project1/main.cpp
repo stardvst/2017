01067f04509960f60b2bc22a5375bcff4bc0acbf
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-02 22:39:45 +0400

    statics are init-ed first, can be defined after main

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,68 +1,30 @@
-#include <functional>
-#include <algorithm>
 #include <iostream>
-#include <cstddef>
-#include <cassert>
-#include <vector>
 
 
 class A {
 public:
-  A() : size(sizeof(A)) {}
-  virtual ~A() {}
-
-  virtual void f() const { std::cout << 1; }
-
-  static bool compare(const A* a, const A* b) {
-    assert(a != 0);
-    assert(b != 0);
-    return a->size < b->size;
-  }
-
+  A(int nn = 0) : n(nn) { std::cout << n; }
 protected:
-  size_t size;
+  int n;
 };
 
 class B : public A {
 public:
-  B() : b(0) { size = sizeof(B); }
-  virtual void f() const { std::cout << 2; }
+  B(int param) : j(param), a(--j), b() { std::cout << j; }
 private:
-  char* b;
-};
-
-class C : public A {
-public:
-  C() { size = sizeof(C); }
-  virtual void f() const { std::cout << 3; }
-private:
-  static int* c;
-};
-
-int* C::c = 0;
-
-struct D {
-  void operator()(A* a) const { delete a; }
+  int j;
+  A a;
+  A b;
+  static A c;
 };
 
 
 int main() {
 
-  std::cout << sizeof(A)  << " " << sizeof(B) << " " << sizeof(C) << "\n";
-
-  typedef std::vector<A*> V;
-  V v;
-  v.push_back(new C);
-  v.push_back(new B);
-  v.push_back(new A);
-
-  std::stable_sort(v.begin(), v.end(), A::compare);
-
-  std::for_each(v.begin(), v.end(), std::mem_fun(&A::f));
-
-  D d;
-  std::for_each(v.begin(), v.end(), d);
+  B b(2);
 
   std::cin.get();
   return 0;
 }
+
+A B::c = 3;
