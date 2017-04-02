6f64c6ea6638cca3e97533b5cd6aac7beedbf0cb
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-02 21:54:46 +0400

    direct vs indirect base class (obscire)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 
 
-struct A {
-  virtual int shift(int n = 2) const { return n << 2; }
+class A {
+public:
+  A(int nn = 2) : n(nn) {}
+
+  int get_n() const { return n; }
+  void set_n(int nn) { n = nn; }
+private:
+  int n;
 };
 
-struct B : A {
-  int shift(int n = 3) const { return n << 3; }
+class B {
+public:
+  B(char cc = 'a') : c(cc) {}
+
+  char get_c() const { return c; }
+  void set_c(char cc) { c = cc; }
+private:
+  char c;
 };
+
+class C : virtual public A, public B {};
+class D : virtual public A, public B {};
+class E : public C, public D {};
 
 
 int main() {
 
-  const B b;
-  const A* a = &b;
+  E e;
+  C& c = e;
+  D& d = e;
 
-  std::cout << a->shift();
+  std::cout << c.get_c() << d.get_n();
+
+  c.set_n(3);
+  d.set_c('b');
+
+  std::cout << c.get_c() << d.get_n();
 
   std::cin.get();
   return 0;
