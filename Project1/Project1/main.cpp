75bb162c974b7984e8491e8ea06e6390f2935728
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-03 17:30:12 +0400

    compilation error - private ctor

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
 
 
-struct A {
-  A() { std::cout << "A()"; }
-  A(int) { std::cout << "A(int)"; }
-  A& operator=(int) { std::cout << "="; return *this; }
-};
-
-class B {
-public: 
-  B() : m2(8) { m1 = 5; }
-private:
-  A m1;
-  A m2;
+class A {
+  int i;
+  A() : i(0) { std::cout << "Hello world"; }
 };
 
 
 int main() {
 
-  B b;
+  A a;
 
   std::cin.get();
   return 0;
