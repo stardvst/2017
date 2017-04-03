e76b3210b9c3d602003209d4c21d9788d4eb52be
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-03 17:42:39 +0400

    useless example

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 
 
-int& foo() {
-  static int a = 3;
-  return a;
-}
-
-
 int main() {
 
-  ++++foo();
-  std::cout << foo();
+  int w = 0;
+  char a[10][10];
+  char* s = "Hello world bye earth";
+
+  for(int i = 0; i < strlen(s); ++i) {
+    if(s[i] == ' ') {
+      int k = 0;
+      for(int j = i; j < strlen(s); ++j) {
+        if(s[j] != ' ') {
+          a[w][k] = s[j];
+          k++;
+          a[w][k] = '\0';
+          i++;
+        } else {
+          break;
+        }
+      }
+    } else {
+      continue;
+    }
+    w++;
+  }
 
   std::cin.get();
   return 0;
