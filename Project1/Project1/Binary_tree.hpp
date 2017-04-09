3b9a754ad9b591789cc15cd22297b661a5a1e324
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-09 15:21:22 +0400

    added iterative versions

--- Project1/Project1/Binary_tree.hpp
+++ Project1/Project1/Binary_tree.hpp
@@ -16,14 +16,17 @@ public:
   void inorder_iter() const;
 
   void postorder() const;
+  void postorder_iter() const;
+
   void preorder() const;
+  void preorder_iter() const;
+
   void levelorder() const;
 
 private:
   void _inorder(Node<T>*) const;
   void _postorder(Node<T>*) const;
   void _preorder(Node<T>*) const;
-  void _levelorder(Node<T>*) const;
 private:
   Node<T>* root;
 };
@@ -32,6 +35,15 @@ private:
 template<typename T>
 void Binary_tree<T>::inorder() const {
   _inorder(root);
+}
+
+template<typename T>
+void Binary_tree<T>::_inorder(Node<T>* ptr) const {
+  if(ptr != 0) {
+    _inorder(ptr->left);
+    std::cout << ptr->value << ' ';
+    _inorder(ptr->right);
+  }
 }
 
 template<typename T>
@@ -52,15 +64,98 @@ void Binary_tree<T>::inorder_iter() const {
   }
 }
 
+
 template<typename T>
 void Binary_tree<T>::postorder() const {
   _postorder(root);
 }
 
 template<typename T>
+void Binary_tree<T>::_postorder(Node<T>* ptr) const {
+  if(ptr != 0) {
+    _postorder(ptr->left);
+    _postorder(ptr->right);
+    std::cout << ptr->value << ' ';
+  }
+}
+
+template<typename T>
+void Binary_tree<T>::postorder_iter() const {
+  std::stack<Node<T>*> stack;
+  Node<T>* node = root;
+
+  while(true) {
+
+    while(node) {
+      stack.push(node);
+      node = node->left;
+    }
+
+    node = stack.top();
+    if(node->right == 0) {
+      stack.pop();
+      std::cout << node->value << ' ';
+
+      Node<T>* top = stack.top();
+      if(!stack.empty() && node == top->right) {
+        std::cout << top->value << ' ';
+        stack.pop();
+      }
+
+    }
+
+    if(stack.empty()) {
+      break;
+    }
+
+    if(stack.top() != 0) {
+      node = stack.top();
+      node = node->right;
+    } else {
+      node = 0;
+    }
+
+  }
+}
+
+
+template<typename T>
 void Binary_tree<T>::preorder() const {
   _preorder(root);
 }
+
+template<typename T>
+void Binary_tree<T>::_preorder(Node<T>* ptr) const {
+  if(ptr != 0) {
+    std::cout << ptr->value << ' ';
+    _preorder(ptr->left);
+    _preorder(ptr->right);
+  }
+}
+
+template<typename T>
+void Binary_tree<T>::preorder_iter() const {
+  if(!root) {
+    return;
+  }
+
+  std::stack<Node<T>*> stack;
+  stack.push(root);
+
+  while(!stack.empty()) {
+    Node<T>* node = stack.top();
+    std::cout << node->value << ' ';
+    stack.pop();
+
+    if(node->right) {
+      stack.push(node->right);
+    }
+    if(node->left) {
+      stack.push(node->left);
+    }
+  }
+}
+
 
 template<typename T>
 void Binary_tree<T>::levelorder() const {
@@ -81,33 +176,6 @@ void Binary_tree<T>::levelorder() const {
         queue.push(node->right);
       }
     }
-  }
-}
-
-template<typename T>
-void Binary_tree<T>::_inorder(Node<T>* ptr) const {
-  if(ptr != 0) {
-    _inorder(ptr->left);
-    std::cout << ptr->value << ' ';
-    _inorder(ptr->right);
-  }
-}
-
-template<typename T>
-void Binary_tree<T>::_postorder(Node<T>* ptr) const {
-  if(ptr != 0) {
-    _postorder(ptr->left);
-    _postorder(ptr->right);
-    std::cout << ptr->value << ' ';
-  }
-}
-
-template<typename T>
-void Binary_tree<T>::_preorder(Node<T>* ptr) const {
-  if(ptr != 0) {
-    std::cout << ptr->value << ' ';
-    _preorder(ptr->left);
-    _preorder(ptr->right);
   }
 }
 
