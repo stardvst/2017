bf8ae0255fd8c84b3b604c3f40f3cc847358f589
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-16 20:27:10 +0400

    threads and mutexes. wow i love this!

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
-#include <iomanip>
 #include <thread>
-#include <chrono>
-#include <ctime>
-using std::chrono::system_clock;
+#include <vector>
+#include <mutex>
+#include <Windows.h>
+
+
+std::vector<int> vec;
+std::mutex mutex;
+
+void push() {
+  mutex.lock();
+  for(int i = 0; i != 10; ++i) {
+    std::cout << "Push " << i << std::endl;
+    Sleep(500);
+    vec.push_back(i);
+  }
+  mutex.unlock();
+}
+
+
+void pop() {
+  mutex.lock();
+  for(int i = 0; i != 10; ++i) {
+    if(vec.size() > 0) {
+      int val = vec.back();
+      vec.pop_back();
+      std::cout << "Pop " << val << std::endl;
+    }
+    Sleep(500);
+  }
+  mutex.unlock();
+}
+
 
 int main() {
 
-  std::cout << "The id of current thread is " << std::this_thread::get_id() << '\n'; // or get_id
-
-  time_t timet = system_clock::to_time_t(system_clock::now());
-
-  tm* time = localtime(&timet);
-
-  std::cout << "Current time: " << std::put_time(time, "%X") << '\n';
-  std::cout << "Waiting for the next minute to begin...\n";
-
-  time->tm_min++;
-  time->tm_sec = 0;
-
-  std::this_thread::sleep_until(system_clock::from_time_t(mktime(time)));
-  std::cout << std::put_time(time, "%X") << " reached!\n";
-
-  std::this_thread::sleep_for(std::chrono::seconds(5));
-
-  timet = system_clock::to_time_t(system_clock::now());
-  time = localtime(&timet);
-  std::cout << "Current time: " << std::put_time(time, "%X") << '\n';
+  std::thread push(push);
+  std::thread pop(pop);
+  if(push.joinable())
+    push.join();
+  if(pop.joinable())
+    pop.join();
 
   std::cin.get();
   return 0;
 }
+
