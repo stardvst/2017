ea84b26a8a9878ceb59b40d1acb658a17adc979d
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-16 20:02:35 +0400

    this_thread namespace

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,93 +1,32 @@
 #include <iostream>
+#include <iomanip>
 #include <thread>
-
-
-void f_thr() {
-  std::cout << "welcome to multithreading!\n";
-}
-
-void print_values(int i, char* c, double d) {
-  std::cout << i << ' ' << c << ' ' << d << std::endl;
-}
-
-class A {
-public:
-  void operator()() {
-    std::cout << "operator()\n";
-  }
-  void other_function() {
-    std::cout << "other function";
-  }
-};
-
-class B {
-public:
-  void operator()(int* arr, int size) {
-    std::cout << "an array of size " << size << " is passed to thread.\n";
-    for(int i = 0; i != size; ++i) {
-      std::cout << arr[i] << ' ';
-    }
-    std::cout << std::endl;
-  }
-  void func_with_params(int* arr, int size) {
-    std::cout << "an array of size " << size << " is passed to thread.\n";
-    for(int i = 0; i != size; ++i) {
-      std::cout << arr[i] << ' ';
-    }
-    std::cout << std::endl;
-    std::cout << "Changing sign of all elements of initial array\n";
-    for(int i = 0; i != size; ++i) {
-      arr[i] *= -1;
-      std::cout << arr[i] << ' ';
-    }
-    std::cout << std::endl;
-  }
-};
+#include <chrono>
+#include <ctime>
+using std::chrono::system_clock;
 
 int main() {
 
-  // std::thread t(f_thr); // welcome to multithreading!
+  std::cout << "The id of current thread is " << std::this_thread::get_id() << '\n'; // or get_id
 
-  // A a;
-  // std::thread t(a); // operator()
-  // std::thread t(&A::other_function, a); // other function
+  time_t timet = system_clock::to_time_t(system_clock::now());
 
-  // char* str = "Hello world!";
-  // std::thread t(print_values, 10, str, 2.5);
+  tm* time = localtime(&timet);
 
-  // B b;
-  // int arr[6] = { 1,2,3,4,5,6 };
-  // std::thread t(b, arr, 6);
-  // std::thread t(&B::func_with_params, b, arr, 6);
+  std::cout << "Current time: " << std::put_time(time, "%X") << '\n';
+  std::cout << "Waiting for the next minute to begin...\n";
 
-  std::thread t1(f_thr);
-  std::thread t2(f_thr);
-  std::thread t3(f_thr);
+  time->tm_min++;
+  time->tm_sec = 0;
 
-  std::thread::id id1 = t1.get_id();
-  std::thread::id id2 = t2.get_id();
-  std::thread::id id3 = t3.get_id();
+  std::this_thread::sleep_until(system_clock::from_time_t(mktime(time)));
+  std::cout << std::put_time(time, "%X") << " reached!\n";
 
-  if(t1.joinable()) {
-    t1.join();
-    std::cout << "Thread with id " << id1 << " is terminated" << std::endl;
-  }
-  if(t2.joinable()) {
-    t2.join();
-    std::cout << "Thread with id " << id2 << " is terminated" << std::endl;
-  }
-  if(t3.joinable()) {
-    t3.join();
-    std::cout << "Thread with id " << id3 << " is terminated" << std::endl;
-  }
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 
-
-  // t.detach(); // t becomes not joinable => else {} executes
-  /*if(t.joinable()) {
-    t.join();
-  } else {
-    std::cout << "t is detached.\n";
-  }*/
+  timet = system_clock::to_time_t(system_clock::now());
+  time = localtime(&timet);
+  std::cout << "Current time: " << std::put_time(time, "%X") << '\n';
 
   std::cin.get();
   return 0;
