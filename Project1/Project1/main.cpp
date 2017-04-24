26d067e2efe89edfc4a2b9cfcde4f658e506b161
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-24 19:18:39 +0400

    task 24: v.2 - uses 2 files, removes original, renames copy

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,57 +1,42 @@
 #include <iostream>
-#include <iomanip>
 #include <fstream>
-#include <sstream>
-#include <utility>
 #include <string>
-#include <list>
-#include <map>
+#include <cstdio>
+#include <set>
 
 
 int main() {
 
-  std::ifstream infile("str_matrix.txt");
+  std::fstream iofile;
+  iofile.open("csv.txt");
 
-  std::map<std::string, std::pair<int, std::list<int> > > lines;
+  if(iofile.is_open()) {
 
-  std::string line;
-  int index = 0;
-  while(std::getline(infile, line)) {
-    if(lines.find(line) != lines.end()) {
-      ++lines[line].first; // ++frequency
-      lines[line].second.push_back(index);
-    } else {
-      lines[line] = std::make_pair<int, std::list<int> >(1, std::list<int>());
-      lines[line].second.push_back(index);
+    std::set<std::string> lines;
+    std::string line;
+
+    std::ofstream copy;
+    copy.open("copy.txt");
+
+    if(copy.is_open()) {
+
+      while(std::getline(iofile, line)) {
+        if(lines.find(line) == lines.end()) {
+          lines.insert(line);
+          copy << line << '\n';
+        }
+      }
+
+      iofile.close();
+      copy.close();
+
+      std::remove("csv.txt");
+      std::rename("copy.txt", "csv.txt");
     }
-    ++index;
+
   }
 
-
-  std::ofstream outfile("unique_m_if.txt", std::ios::trunc);
-  outfile
-    << std::setw(11) << "word" << std::setw(9) << '|'
-    << std::setw(9) << "freq." << std::setw(5) << '|'
-    << std::setw(20) << "indexes" << std::setw(12) << '|'
-    << "\n------------------------------------------------------------------\n";
-  for(std::map<std::string, std::pair<int, std::list<int> > >::const_iterator
-      it = lines.begin(); it != lines.end(); ++it) {
-
-    outfile
-      << std::setw(11) << it->first
-      << std::setw(16) << it->second.first
-      << std::setw(14);
-
-    std::list<int>::const_iterator i = it->second.second.begin();
-    std::list<int>::const_iterator list_end = --(it->second.second.end());
-    for(; i != list_end; ++i) {
-      outfile << *i << ", ";
-    }
-    outfile << *(--(it->second.second.end())) << '\n';
-  }
-
-
-  std::cout << "see \"unique_m_if.txt\" for the result.";
+  std::cout << "see \"csv.txt\" for the result.";
 
   std::cin.get();
   return 0;
