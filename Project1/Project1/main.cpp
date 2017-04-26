3c0b370cc7b7592dfb032e8e6a0a531098d1448a
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-26 11:50:15 +0400

    task #21: print words that appear only in 1 column

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
-#include "median.hpp"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <set>
 
 
 int main() {
 
-  Median<int> median("numbers.txt");
-  
-  std::cout << "median: " << median.get() << std::endl;
+  std::ifstream infile;
+  infile.open("words.txt");
+
+  if(infile.is_open()) {
+    std::map<std::string, std::set<int> > words;
+
+    std::string row;
+    while(std::getline(infile, row)) {
+      
+      int column = 0;
+      std::string word;
+      std::istringstream line(row);
+      while(line >> word) {
+        words[word].insert(column++);
+      }
+
+    }
+
+    for(std::map<std::string, std::set<int> >::const_iterator it = words.begin();
+        it != words.end(); ++it) {
+      if(it->second.size() == 1) { // word appears only in 1 column
+        std::cout << it->first << '\n';
+      }
+    }
+
+    infile.close();
+  }
 
 
   std::cin.get();
