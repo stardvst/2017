6318fb214dc4b2a9024d2231b982b8abe859b2cb
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-27 18:57:01 +0400

    task #8: kurtosis (single pass)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <cmath>
 
 
 int main() {
@@ -11,20 +10,33 @@ int main() {
   if(infile.is_open() && infile.peek() != std::ifstream::traits_type::eof()) {
 
     int n = 0;
-    double sum = 0;
-    double sum_of_squares = 0;
+    double mean = 0; // M1
+    double M2 = 0;
+    double M3 = 0;
+    double M4 = 0;
 
     double value;
     while(infile >> value) {
-      sum_of_squares += value * value;
-      sum += value;
+      double delta;
+      double delta_n;
+      double delta_n2;
+
+      int n1 = n;
       ++n;
+
+      delta = value - mean;
+      delta_n = delta / n;
+      delta_n2 = delta_n * delta_n;
+
+      double term1 = delta * delta_n * n1;
+
+      mean += delta_n;
+      M4 += term1 * delta_n2 * (n * n - 3 * n + 3) + 6 * delta_n2 * M2 - 4 * delta_n * M3;
+      M3 += term1 * delta_n * (n - 2) - 3 * delta_n * M2;
+      M2 += term1;
     }
 
-    double mean = sum / n;
-
-    std::cout << "sample standard deviation: "
-      << std::sqrt((sum_of_squares - n * mean * mean) / (n - 1));
+    std::cout << "kurtosis: " << (n * M4) / (M2 * M2) - 3;
 
 
     infile.close();
