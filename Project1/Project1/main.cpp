87239a021ad80ab4a23ded60ff90a1417f2f2054
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-04-27 19:30:08 +0400

    task #7: skewness (single pass)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 
 int main() {
@@ -13,30 +14,26 @@ int main() {
     double mean = 0; // M1
     double M2 = 0;
     double M3 = 0;
-    double M4 = 0;
 
     double value;
     while(infile >> value) {
       double delta;
       double delta_n;
-      double delta_n2;
 
       int n1 = n;
       ++n;
 
       delta = value - mean;
       delta_n = delta / n;
-      delta_n2 = delta_n * delta_n;
 
       double term1 = delta * delta_n * n1;
 
       mean += delta_n;
-      M4 += term1 * delta_n2 * (n * n - 3 * n + 3) + 6 * delta_n2 * M2 - 4 * delta_n * M3;
       M3 += term1 * delta_n * (n - 2) - 3 * delta_n * M2;
       M2 += term1;
     }
 
-    std::cout << "kurtosis: " << (n * M4) / (M2 * M2) - 3;
+    std::cout << "kurtosis: " << (std::sqrt(n) * M3) / std::pow(M2, 1.5);
 
 
     infile.close();
