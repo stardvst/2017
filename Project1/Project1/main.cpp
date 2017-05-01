d8b690bb9a6e037bd5bc50620acb412a51117c2e
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-05-01 21:52:21 +0400

    task #5: nth percentile (using vector, std::nth_element)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,3 +1,48 @@
+//#include <iostream>
+//#include <fstream>
+//#include "quicksort.hpp"
+//
+//
+//int main() {
+//
+//  std::ifstream file;
+//  file.open("numbers.txt");
+//  
+//  Quicksort<int> q(file);
+//  file.close();
+//
+//  q.multi_threaded();
+//  q.print();
+//  
+//  std::cin.get();
+//  return 0;
+//}
+
+//
+//#include <iostream>
+//#include "red_black_tree.hpp"
+//#include "Node.hpp"
+//#include <set>
+//
+//
+//int main() {
+//
+//    RedBlackTree<int> rbt;
+//    rbt.insert(7);
+//    rbt.insert(11);
+//    rbt.insert(20);
+//    rbt.insert(40);
+//    rbt.insert(15);
+//    rbt.insert(45);
+//
+//    rbt.remove(15);
+//
+//
+//    std::cin.get();
+//    return 0;
+//}
+
+
 #include <algorithm>
 #include <iostream>
 #include <fstream>
@@ -18,16 +63,14 @@ int main() {
             v.push_back(value);
         }
 
-        std::sort(v.begin(), v.end());
-
         int n = 0;
         while(n <= 0 || n > 100) {
             std::cout << "calculating nth percentile, n = ";
             std::cin >> n;
         }
 
-
-        std::cout << "nth percentile: " << v[static_cast<int>(n * v.size() / 100) - 1];
+        std::nth_element(v.begin(), v.begin() + n * v.size() / 100, v.end());
+        std::cout << "nth percentile: " << v[n * v.size() / 100];
 
         file.close();
     }
