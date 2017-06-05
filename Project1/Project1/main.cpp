89f479cb191433e82b6d346675aa0ad109302bff
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-05 09:46:57 +0400

    timus - sqrts in reverse order, ugh

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
-#include "graph.hpp"
+#include <iomanip>
+#include <stack>
+#include <cmath>
 
 
 int main() {
 
-    int vertices;
-    std::cout << "number of vertices: ";
-    std::cin >> vertices;
-
-    Graph graph(vertices);
-
-    std::cout << "\nedges (ctrl+z to exit):\n";
-    while(true) {
-        int v1;
-        int v2;
-        std::cin >> v1;
-        if(!std::cin.good()) {
-            break;
-        }
-        std::cin >> v2;
-        if(v1 >= 0 && v1 < vertices && v2 >= 0 && v2 < vertices) {
-            graph.add_edge(v1, v2);
-        }
+    std::stack<double> numbers;
+    double number;
+    while(std::cin >> number) {
+        numbers.push(number);
     }
 
-    std::cout << "\nis connected? " << std::boolalpha << graph.is_connected()
-        << "\nedge connectivity: " << graph.edge_connectivity();
+    while(!numbers.empty()) {
+        std::cout << std::fixed << std::setprecision(4) << std::sqrt(numbers.top()) << '\n';
+        numbers.pop();
+    }
 
     std::cin.get();
     return 0;
