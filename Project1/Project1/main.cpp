e30c99cca5cec409b27ad9f948f15a8394324e56
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-07 20:47:49 +0400

    random search algorithm

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <random>
 
 
-class A {
-public:
-    virtual void f() { std::cout << "A::f"; }
-    A() { f(); }
-    /* virtual */ ~A() { f(); }
-};
+int RANDOM_SEARCH(const std::vector<int>& A, int x) {
+    const int n = static_cast<int>(A.size());
+    std::vector<bool> C(n, false);
+    int picked = 0;
+    std::random_device rd;
+    std::mt19937_64 mt(rd());
+    std::uniform_int_distribution<> dist(0, n - 1);
+    while(true) {
+        int i = dist(mt);
+        if(C[i] == false) {
+            C[i] = true;
+            ++picked;
+            if(picked == n) {
+                return -1;
+            } else if(A[i] == x) {
+                return i;
+            }
+        }
+    }
+}
 
-
-class B : public A {
-public:
-    void f() override { std::cout << "B::f"; }
-    B() { f(); }
-    ~B() { f(); }
-};
 
 int main() {
 
-    A* p = new B;
-    p->f();
-    delete p;
-
+    std::vector<int> A { 3,1,6,8,7,5,3 };
+    std::cout << RANDOM_SEARCH(A, 3);
+    
     std::cin.get();
     return 0;
 }
