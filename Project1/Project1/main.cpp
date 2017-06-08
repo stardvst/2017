8ef6291f939a65f4887017282a030d9c4e121384
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-08 09:55:50 +0400

    simultaneous min and max

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,30 +1,43 @@
+#include <algorithm>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 
-void COMPARE_EXCHANGE(std::vector<int>& A, int i, int j) {
-    if(A[i] > A[j]) {
-        std::swap(A[i], A[j]);
-    }
-}
+std::pair<int, int> simultaneous_min_max(const std::vector<int>& v) {
+    const std::size_t size = v.size();
+    
+    int min = v[0];
+    int max = min;
+    int i = 1;
 
-void INSERTION_SORT(std::vector<int>& A) {
-    const int size = static_cast<int>(A.size());
-    for(int j = 2; j < size; ++j) {
-        for(int i = j - 1; i >= 0; --i) {
-            COMPARE_EXCHANGE(A, i, i + 1);
+    if(size % 2 == 0) {
+        min = std::min(min, v[1]);
+        max = std::max(max, v[1]);
+        i = 2;
+    }
+
+    for(; i < size - 2; i += 2) {
+        int current_min = std::min(v[i], v[i + 1]);
+        int current_max = std::max(v[i], v[i + 1]);
+        if(current_min < min) {
+            min = current_min;
+        }
+        if(current_max > max) {
+            max = current_max;
         }
     }
+
+    return std::make_pair(min, max);
 }
 
 
 int main() {
 
     std::vector<int> A { 3,1,6,8,7,5,3 };
-    INSERTION_SORT(A);
-    for(std::size_t i = 0; i < 7; ++i) {
-        std::cout << A[i] << ' ';
-    }
+    std::pair<int, int> pair = simultaneous_min_max(A);
+    std::cout << "min: " << pair.first << '\n';
+    std::cout << "max: " << pair.second << std::endl;
     
     std::cin.get();
     return 0;
