2e4c4035d520aa95fde2bb1a116ddd37e5208d85
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-08 15:45:58 +0400

    12-hour to 24-hour conversion

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,52 +1,83 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <limits>
-#include <tuple>
+#include <regex>
 
+using namespace std;
 
-std::tuple<int, int, int> simultaneous_min_max(const std::vector<int>& v) {
-    const std::size_t size = v.size();
+string timeConversion(string s) {
+    regex regex { R"((\d{2}):(\d{2}):(\d{2})(\w{2}))" };
+    smatch info;
+    regex_match(s, info, regex);
 
-    int min1 = v[0];
-    int min2 = std::numeric_limits<int>::max();
-    int max = min1;
-    int i = 1;
+    string result;
 
-    if(size % 2 == 0) {
-        min1 = std::min(min1, v[1]);
-        max = std::max(max, v[1]);
-        min2 = max;
-        i = 2;
-    }
+    int hour = std::stoi(info[1]);
+    int min = std::stoi(info[2]);
+    int sec = std::stoi(info[3]);
+    string ampm = info[4];
 
-    for(; i < size - 1; i += 2) {
-        int current_min = std::min(v[i], v[i + 1]);
-        int current_max = std::max(v[i], v[i + 1]);
-        if(current_min < min1) {
-            if(min1 < min2) {
-                min2 = min1;
+    if(hour >= 1 && hour <= 12 &&
+       min >= 0 && min <= 59 &&
+       sec >= 0 && sec <= 59) {
+        if(ampm == "AM") {
+            if(hour == 12) {
+                result += "00:";
+            } else {
+                result += info[1];
+                result += ":";
             }
-            min1 = current_min;
-        } else if(current_min < min2) {
-            min2 = current_min;
+        } else {
+            switch(hour) {
+                case 1:
+                    result += "13:";
+                    break;
+                case 2:
+                    result += "14:";
+                    break;
+                case 3:
+                    result += "15:";
+                    break;
+                case 4:
+                    result += "16:";
+                    break;
+                case 5:
+                    result += "17:";
+                    break;
+                case 6:
+                    result += "18:";
+                    break;
+                case 7:
+                    result += "19:";
+                    break;
+                case 8:
+                    result += "20:";
+                    break;
+                case 9:
+                    result += "21:";
+                    break;
+                case 10:
+                    result += "22:";
+                    break;
+                case 11:
+                    result += "23:";
+                    break;
+                case 12:
+                    result += "12:";
+                    break;
+            }
         }
-        if(current_max > max) {
-            max = current_max;
-        }
+        result += info[2];
+        result.append(":");
+        result += info[3];
     }
 
-    return std::make_tuple(min1, min2, max);
+    return result;
 }
 
-
 int main() {
-
-    std::vector<int> A { 3,0,1,5,4,7,10,8,9,12 };
-    std::tuple<int, int, int> tuple = simultaneous_min_max(A);
-    std::cout << "min: " << std::get<0>(tuple) << '\n'
-        << "2nd min: " << std::get<1>(tuple) << '\n'
-        << "max: " << std::get<2>(tuple) << std::endl;
+    string s;
+    cin >> s;
+    string result = timeConversion(s);
+    cout << result << endl;
 
     std::cin.get();
     return 0;
