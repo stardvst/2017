ad7b0bd0df44dfd396e7fc32d4128395849912e1
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-12 14:53:42 +0400

    algo exam: O(logn + logm) solution that wasn't good enough since this wasn't the solution written on the internet :( (works perfectly!)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,84 +1,89 @@
 #include <iostream>
-#include <regex>
+#include <utility>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-using namespace std;
 
-string timeConversion(string s) {
-    regex regex { R"((\d{2}):(\d{2}):(\d{2})(\w{2}))" };
-    smatch info;
-    regex_match(s, info, regex);
-
-    string result;
-
-    int hour = std::stoi(info[1]);
-    int min = std::stoi(info[2]);
-    int sec = std::stoi(info[3]);
-    string ampm = info[4];
-
-    if(hour >= 1 && hour <= 12 &&
-       min >= 0 && min <= 59 &&
-       sec >= 0 && sec <= 59) {
-        if(ampm == "AM") {
-            if(hour == 12) {
-                result += "00:";
-            } else {
-                result += info[1];
-                result += ":";
-            }
-        } else {
-            switch(hour) {
-                case 1:
-                    result += "13:";
-                    break;
-                case 2:
-                    result += "14:";
-                    break;
-                case 3:
-                    result += "15:";
-                    break;
-                case 4:
-                    result += "16:";
-                    break;
-                case 5:
-                    result += "17:";
-                    break;
-                case 6:
-                    result += "18:";
-                    break;
-                case 7:
-                    result += "19:";
-                    break;
-                case 8:
-                    result += "20:";
-                    break;
-                case 9:
-                    result += "21:";
-                    break;
-                case 10:
-                    result += "22:";
-                    break;
-                case 11:
-                    result += "23:";
-                    break;
-                case 12:
-                    result += "12:";
-                    break;
-            }
+int get_col(const std::vector<int>& matrix_row0, int key) {
+    int low = 0;
+    int high = static_cast<int>(matrix_row0.size()) - 1;
+    int col1 = 0;
+    int col2 = 0;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(matrix_row0[mid] == key) {
+            return mid;
         }
-        result += info[2];
-        result.append(":");
-        result += info[3];
+        if(matrix_row0[mid] < key) {
+            col1 = mid;
+            low = mid + 1;
+        } else if(matrix_row0[mid] > key) {
+            col2 = mid;
+            high = mid - 1;
+        }
     }
-
-    return result;
+    return col1;
 }
 
-int main() {
-    string s;
-    cin >> s;
-    string result = timeConversion(s);
-    cout << result << endl;
+int get_row(const std::vector<std::vector<int> >& matrix, int col, int key) {
+    int low = 0;
+    int high = static_cast<int>(matrix[0].size()) - 1;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        int element = matrix[mid][col];
+        if(element == key) {
+            return mid;
+        }
+        if(element < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
 
+std::pair<int, int> get_indices(const std::vector<std::vector<int> >& matrix, int key) {
+    int col = get_col(matrix[0], key);
+    int row = get_row(matrix, col, key);
+
+    if(row != -1) {
+        return std::make_pair(row, col);
+    }
+    return std::make_pair(-1, -1);
+}
+
+
+int main() {
+    
+    std::ifstream file;
+    file.open("matrix.txt");
+
+    if(file.is_open() && file.peek() != std::ifstream::traits_type::eof()) {
+        std::vector<std::vector<int> > matrix;
+       
+        std::string row;
+        while(std::getline(file, row)) {
+            matrix.emplace_back(std::vector<int>());
+
+            int number;
+            std::size_t size = matrix.size() - 1;
+            std::istringstream iss(row);
+            while(iss >> number) {
+                matrix[size].emplace_back(number);
+            }
+        }
+
+        int key;
+        std::cout << "key: ";
+        std::cin >> key;
+        std::pair<int, int> indices = get_indices(matrix, key);
+        std::cout << "i:" << indices.first << ", j: " << indices.second << std::endl;
+    }
+
+    std::cin.get();
     std::cin.get();
     return 0;
 }
