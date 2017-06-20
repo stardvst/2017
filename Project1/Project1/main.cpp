b21d22d31c8b6a1b9cc1fe9e5f02f2d665620046
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-06-20 12:30:42 +0400

    longest subsequence of 0s in array

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,37 +1,27 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <limits>
 
 
-int find_2nd_max(const std::vector<int>& v) {
-    const int size = static_cast<int>(v.size());
+int longest_0s(const std::vector<int>& v) {
+    const std::size_t size = v.size();
     
-    int max1 = v[0];
-    int max2 = std::numeric_limits<int>::min();
-    int i = 1;
-    
-    if(!(size & 1)) {
-        max1 = std::max(max1, v[1]);
-        max2 = std::min(v[0], v[1]);
-        i = 2;
-    }
-
-    for(; i < size - 1; i += 2) {
-        int current_max = std::max(v[i], v[i + 1]);
-        if(current_max > max1) {
-            max2 = max1;
-            max1 = current_max;
+    int max = 0;
+    int current_max = 0;
+    for(int i = 0; i < size; ++i) {
+        if(v[i] == 0) {
+            ++current_max;
+        } else if(current_max > max) {
+            max = current_max;
+            current_max = 0;
         }
     }
-
-    return max2;
+    return max;
 }
 
 int main() {
 
-    std::vector<int> v { 3,0,1,5,4,7,10,8,9,12 };
-    std::cout << find_2nd_max(v);
+    std::vector<int> v { 5,0,0,1,0,0,0,2 };
+    std::cout << longest_0s(v);
 
     std::cin.get();
 }
