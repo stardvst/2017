2de9951a200dbc47d18fcc6239e6ad9c93e90f00
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-08-29 21:53:14 +0400

    passing shatred_ptr

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,27 +1,31 @@
-#include <string>
+#include <iostream>
+#include <memory>
 
-struct S {
-    S(std::string s) : m_s(std::move(s)) {}
-    std::string m_s;
-};
+struct Base { virtual int value() const { return 1; } };
+struct Derived : Base { int value() const { return 2; } };
+
+//// don't do this
+//int use_a_base(std::shared_ptr<Base> p) {
+//    return p->value();
+//}
+//
+//int main() {
+//    auto ptr = std::make_shared<Derived>();
+//    use_a_base(ptr);
+//}
+
+// pass a & to const
+int use_a_base(const Base &p) {
+    return p.value();
+}
+
+// pass a * to const
+int use_a_base(const Base *p) {
+    return p->value();
+}
 
 int main() {
-
-    // copy the string
-    for(int i = 0; i < 10000000; ++i) {
-        std::string s = std::string("a not very short string") + "b";
-        S o(s);
-    }
-
-    // move the string: 29% more efficient, 32% smaller binary
-    for(int i = 0; i < 10000000; ++i) {
-        std::string s = std::string("a not very short string") + "b";
-        S o(std::move(s));
-    }
-
-    // CONSTRUCT IN PLACE!!! +2% efficient
-    for(int i = 0; i < 10000000; ++i) {
-        S o("a not very short string") + "b");
-    }
-
+    auto ptr = std::make_shared<Derived>();
+    std::cout << use_a_base(*ptr.get()) << '\n';
+    std::cout << use_a_base(ptr.get()) << '\n';
 }
