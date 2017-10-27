cf3f75ac8d35baedd41fb7bcc34d4547bdc4deb6
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-10-28 00:16:36 +0400

    DESIGN PATTERNS: composite (containers)

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,57 +1,47 @@
 #include <iostream>
+#include <vector>
 
-struct burger {
-    virtual void desc() = 0;
-    virtual int cost() = 0;
+struct Component {
+    virtual void traverse() = 0;
 };
 
-struct french : burger {
-    void desc() { std::cout << "french burger with"; }
-    int cost() { return 100; }
-};
-
-struct italian : burger {
-    void desc() { std::cout << "italian burger with"; }
-    int cost() { return 120; }
-};
-
-struct decorator : burger {
-    decorator(burger *b) : m_burger(b) {}
-    void desc() { m_burger->desc(); }
-    int cost() { return m_burger->cost(); }
+struct Leaf : Component {
+    Leaf(int val) : v(val) {}
+    void traverse() { std::cout << v << ' '; }
 private:
-    burger *m_burger;
+    int v;
 };
 
-struct cheese : decorator {
-    cheese(burger *b) : decorator(b) {}
-    void desc() {
-        decorator::desc();
-        std::cout << " cheese";
+struct Composite : Component {
+    void add(Component *c) { children.push_back(c); }
+    void traverse() {
+        for(std::size_t i = 0; i < children.size(); ++i) {
+            children[i]->traverse();
+        }
     }
-    int cost() { return decorator::cost() + 150; }
-};
-
-struct sauce : decorator {
-    sauce(burger *b) : decorator(b) {}
-    void desc() {
-        decorator::desc();
-        std::cout << " sauce";
-    }
-    int cost() { return decorator::cost() + 200; }
+private:
+    std::vector<Component *> children;
 };
 
 int main() {
 
-    burger *b = new sauce(
-        new cheese(
-            new french
-        )
-    );
+    Composite containers[4];
+    
+    for(int i = 0; i < 4; ++i) {
+        for(int j = 0; j < 3; ++j) {
+            containers[i].add(new Leaf(i * 3 + j));
+        }
+    }
 
-    b->desc();
+    for(int i = 1; i < 4; ++i) {
+        containers[0].add(&containers[i]);
+    }
 
-    std::cout << "\ncost: " << b->cost() << " dram";
+    for(int i = 0; i < 4; ++i) {
+        containers[i].traverse();
+        std::cout << '\n';
+    }
+
     std::cin.get();
     return 0;
 }
