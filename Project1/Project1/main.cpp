4deb96ecc3cebdacbf0ff38746afbe7cb86533a4
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-10-28 19:37:46 +0400

    async

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -9,15 +9,21 @@ int func(int n) {
     return n;
 }
 
+struct fnc_ob {
+    int n;
+    fnc_ob(int i) : n(i) {}
+    int operator()() { return n; }
+};
+
 int main() {
 
-    packaged_task<int(int)> f(func); // assign a function to p_t
-    future<int /*ret val*/> r = f.get_future(); // link a future to ret val of p_t f
+    future<int> f5 = async(func, 5);
+    future<int> f10 = async(fnc_ob(10));
+    auto f20 = async([](int i) {return i; }, 20);
 
-    thread t1(move(f), 9); // no copy ctor so it must be moved (to keep uniquness of p_t)
-    cout << r.get() << endl;
-
-    t1.join();
+    cout << f5.get() << endl;
+    cout << f10.get() << endl;
+    cout << f20.get() << endl;
 
     std::cin.get();
     return 0;
