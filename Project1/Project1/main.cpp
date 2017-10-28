26e6f4a167050681d4c3012b9b8f5282b28be579
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-10-28 19:20:03 +0400

    promise - future example

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,77 +1,28 @@
 #include <iostream>
-#include <condition_variable>
-#include <mutex>
-#include <queue>
+#include <thread>
+#include <future>
 #include <random>
 
 using namespace std;
 
-queue<char> recvq;
-condition_variable recv_cond;
-mutex recv_mutex;
-
-queue<char> sendq;
-condition_variable send_cond;
-mutex send_mutex;
-
-void producer() {
-    default_random_engine eng;
-    
-    uniform_int<char> chardist {'a', 'z'};
-    while(true) {
-        char c = chardist(eng);
-        
-        unique_lock<mutex> recv_lock { recv_mutex };
-        recvq.push(c);
-        recv_cond.notify_one(); // notify modifier
-        recv_lock.unlock(); // unlock explicitly
-
-        this_thread::sleep_for(chrono::milliseconds(100)); 
-    }
-}
-
-void modifier() {
-    while(true) {
-        unique_lock<mutex> recv_lock { recv_mutex };
-        
-        while(recvq.empty())
-            recv_cond.wait(recv_lock); // wait for a notif from producer
-        
-        char c = recvq.front();
-        recvq.pop(); // the modification we need the lock for
-        recv_lock.unlock();
-        
-        c -= 32; // to uppercase
-        
-        unique_lock<mutex> send_lock { send_mutex };
-        sendq.push(c);
-        send_cond.notify_one();
-        // not calling unlock
-    } // send_lock is released
-}
-
-void consumer() {
-    while(true) {
-        unique_lock<mutex> send_lock { send_mutex };
-        
-        while(sendq.empty())
-            send_cond.wait(send_lock); // wait for a notif from modifier
-
-        char c = sendq.front();
-        sendq.pop();
-        cout << c << endl;
-    } // send_lock is released
+void consumer(future<char> &fx) {
+    char c = fx.get();
+    cout << c << endl;
 }
 
 int main() {
 
-    thread t1 { producer };
-    thread t2 { modifier };
-    thread t3 { consumer };
+    promise<char> px;
+    future<char> fx = px.get_future();
+
+    thread t1(consumer, ref(fx));
+
+    default_random_engine eng;
+    uniform_int<char> dist { 'a', 'z' };
+
+    px.set_value(dist(eng));
 
     t1.join();
-    t2.join();
-    t3.join();
 
     std::cin.get();
     return 0;
