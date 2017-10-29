b5202daf2127ee649bf4d40c3763e6d2988b1ecc
Lilia Manukyan <lilia.manukyan@hotmail.com>
2017-10-29 14:30:45 +0400

    c++14 regular function type deducation

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,17 +2,15 @@
 
 using namespace std;
 
+auto func(int x, int y) {
+    return x + y;
+}
 
 int main() {
 
     int a = 1, b = 1;
-    double r = 1.0, s = 1.0;
-    //double e, f;
 
-    auto ff = [](auto x, auto y) { return x + y; };
-    auto f3 = [e = 2, f = 2]() {return e*f; };
-
-    cout << ff(a, b) << '\n' << ff(r, s) << '\n' << f3();
+    cout << func(a, b);
     
     std::cin.get();
     return 0;
